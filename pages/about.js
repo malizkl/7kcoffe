51e6bbc04c48da620a71d7bad82feb73043de9a1
malizkl <63977569+malizkl@users.noreply.github.com>
2022-07-27 02:36:29 +0300

    11th commit

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ function AboutPage(){
 
     <p className="py-4 text-gray-500">Legend has it that Istanbul was built on 7 Hills, like the Byzantine model of Rome.
 
-We intend to set our marks from here to the 7 continents with our Premium High-Quality Gourmet Coffee! 🇹   </p>
+We intend to set our marks from here to the 7 continents with our Premium High-Quality Gourmet Coffee!</p>
 
         <a
         href="https://www.instagram.com/7kcoffeeworkshop/"
